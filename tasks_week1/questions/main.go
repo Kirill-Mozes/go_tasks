6b67a2382b0a857d678642bccea1140979eb1b65
Kirill-Mozes <mozin.kirill@yandex.ru>
2021-07-05 20:24:24 +0300

    question

--- tasks_week1/questions/main.go
+++ tasks_week1/questions/main.go
@@ -1,9 +1,51 @@
 package main
 
-func main() {
-	bs := make([]byte, 10)
-	bl := 0
-	for n := 0; n < 10; n++ {
-		bl += copy(bs[bl:], "x")
+import "fmt"
+
+//Написать объединение двух строк
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
 	}
+	return a
+}
+func unification(a, b string) string {
+	counterA := make(map[rune]int)
+	counterB := make(map[rune]int)
+	var result []rune
+
+	for _, elem := range a { //алфавит строки а
+		if _, ok := counterA[elem]; !ok {
+			counterA[elem] = 1 // если встретили в первый раз
+		} else {
+			counterA[elem] += 1
+		}
+	}
+
+	for _, elem := range b { //алфавит строки б
+		if _, ok := counterB[elem]; !ok {
+			counterB[elem] = 1 // если встретили в первый раз
+		} else {
+			counterB[elem] += 1
+		}
+	}
+	for key, value := range counterA { //объединяем алфавиты
+		if _, ok := counterB[key]; !ok {
+			counterB[key] = value // если встретили в первый раз
+		} else {
+			counterB[key] = abs(counterB[key] - counterA[key]) //пересечение
+		}
+	}
+	for key, value := range counterB {
+		for i := 0; i < value; i++ {
+			result = append(result, key)
+		}
+	}
+	return string(result)
+}
+func main() {
+	a := "aaab"
+	b := "aaaab"
+	fmt.Printf("%v\n", unification(a, b))
 }
